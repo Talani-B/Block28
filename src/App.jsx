cfe039c93da2a0ce1e0b8118eeec8de166aa2e66
Talani Balogun <Taloni5992@gmail.com>
2023-07-31 19:56:22 -0400

    intial files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Routes, Route , Link} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import './App.css'
 import Red from './components/Red';
 import Blue from './components/Blue';
@@ -12,22 +12,13 @@ function App() {
   return (
 
       <div id="container">
-      <div id="navbar">
-       <li>
-        <Link to="/blue">Blue</Link>
-        </li>
-        <li>
-         <Link to="/red">Red</Link>
-         </li>
-        <li>
-        <Link to="/home">Home</Link>
-       </li>
+      <div>
       </div>
       <div id="main-section">
       <Routes>
+      <Route path="/" element={ <Home/> } />
       <Route path="/blue" element={ <Blue/> } />
       <Route path="/red" element={ <Red/> } />
-      <Route path="/home" element={ <Home/> } />
       </Routes> 
 
    </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import  { Link } from 'react-router-dom';
+
+export default function Navbar() {
+    return (
+    <div id="navbar">
+    <Link to="/blue">Blue</Link>
+    <Link to="/red">Red</Link>
+    <Link to="/home">Home</Link>
+       </div>
+    );
+  }
